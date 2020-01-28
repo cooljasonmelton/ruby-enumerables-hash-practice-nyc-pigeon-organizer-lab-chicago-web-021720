6a94dc104fc71db92b94f452a42fa0f0fcaee5c4
Jason Melton <jason.melton2@gmail.com>
2020-01-28 23:10:54 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,9 +1,17 @@
 def nyc_pigeon_organizer(data)
   hash_by_pigeon = data.reduce({}) do |memo, (key, value)|
-    
+    memo 
     
     
   end 
   
   hash_by_pigeon
 end
+
+
+
+
+
+# collect all the names and make them hashes 
+# find the names and give the correct subcategories to them 
+# :color :gender :lives 
