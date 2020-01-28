bf3d1afbdc07aecc027358ed98674a3cb0df4ef0
Jason Melton <jason.melton2@gmail.com>
2020-01-28 23:16:50 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,12 @@
 def nyc_pigeon_organizer(data)
+  new = {}
+  
+  
+  
+  
   hash_by_pigeon = data.reduce({}) do |memo, (key, value)|
-    memo 
     
+
     
   end 
   
@@ -14,4 +19,7 @@ end
 
 # collect all the names and make them hashes 
 # find the names and give the correct subcategories to them 
-# :color :gender :lives 
+# names => "Theo", "Peter Jr.", "Lucky", "Ms. K", "Queenie", "Andrew", "Alex"
+# color => purple, grey, white, brown
+# gender => male, female 
+# lives => "Subway", "Central Park", "Library" "City Hall"
